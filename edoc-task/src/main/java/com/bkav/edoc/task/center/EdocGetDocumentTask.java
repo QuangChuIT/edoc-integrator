49b78ec2a55188159654a85724f34b918c0b2fa4
QuangChuIT <chuvanquang96@gmail.com>
2021-04-20 16:54:58 +0700

    update all:

--- edoc-task/src/main/java/com/bkav/edoc/task/center/EdocGetDocumentTask.java
+++ edoc-task/src/main/java/com/bkav/edoc/task/center/EdocGetDocumentTask.java
@@ -1,5 +1,7 @@
 package com.bkav.edoc.task.center;
 
+import com.bkav.edoc.service.kernel.util.GetterUtil;
+import com.bkav.edoc.task.util.PropsUtil;
 import org.apache.log4j.Logger;
 import org.apache.synapse.ManagedLifecycle;
 import org.apache.synapse.core.SynapseEnvironment;
@@ -13,6 +15,8 @@ public class EdocGetDocumentTask implements Task, ManagedLifecycle {
      */
     @Override
     public void execute() {
+        boolean isTurnOn = GetterUtil.getBoolean(PropsUtil.get("edoc.task.get.document.vpcp.turn.on"), false);
+
         LOGGER.info("------------------------ Edoc Get Documents Task Invoke -----------------------");
     }
 

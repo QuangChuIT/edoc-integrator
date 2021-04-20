9c989eb73ada315f72a1206b6611b379eb156eda
QuangChuIT <chuvanquang96@gmail.com>
2021-04-20 17:34:32 +0700

    update all

--- edoc-service/src/main/java/com/bkav/edoc/service/vpcp/ServiceVPCP.java
+++ edoc-service/src/main/java/com/bkav/edoc/service/vpcp/ServiceVPCP.java
@@ -1,23 +1,38 @@
 package com.bkav.edoc.service.vpcp;
 
 import com.bkav.edoc.service.database.cache.AttachmentCacheEntry;
+import com.bkav.edoc.service.database.entity.EdocDocument;
+import com.bkav.edoc.service.database.entity.EdocDynamicContact;
+import com.bkav.edoc.service.database.util.EdocDocumentServiceUtil;
+import com.bkav.edoc.service.database.util.EdocDynamicContactServiceUtil;
 import com.bkav.edoc.service.kernel.util.GetterUtil;
+import com.bkav.edoc.service.util.CommonUtil;
 import com.bkav.edoc.service.util.PropsUtil;
 import com.bkav.edoc.service.xml.base.Content;
+import com.bkav.edoc.service.xml.base.attachment.Attachment;
 import com.bkav.edoc.service.xml.base.builder.BuildException;
+import com.bkav.edoc.service.xml.base.header.Error;
+import com.bkav.edoc.service.xml.base.header.Organization;
 import com.bkav.edoc.service.xml.base.header.TraceHeaderList;
+import com.bkav.edoc.service.xml.base.parser.ParserException;
+import com.bkav.edoc.service.xml.ed.Ed;
 import com.bkav.edoc.service.xml.ed.header.MessageHeader;
+import com.bkav.edoc.service.xml.ed.parser.EdXmlParser;
 import com.bkav.edoc.service.xml.status.builder.StatusXmlBuilder;
 import com.bkav.edoc.service.xml.status.header.MessageStatus;
 import com.vpcp.services.KnobstickServiceImp;
 import com.vpcp.services.VnptProperties;
-import com.vpcp.services.model.GetChangeStatusResult;
-import com.vpcp.services.model.MessageType;
-import com.vpcp.services.model.SendEdocResult;
+import com.vpcp.services.model.*;
 import org.apache.log4j.Logger;
 import org.json.JSONObject;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ServiceVPCP {
 
@@ -26,6 +41,8 @@ public class ServiceVPCP {
     private final KnobstickServiceImp knobstickServiceImp;
 
     private static final VnptProperties vnptProperties;
+
+    private static final String pStart = "---------";
 
     static public ServiceVPCP getInstance() {
         if (INSTANCE == null) {
@@ -73,6 +90,147 @@ public class ServiceVPCP {
         header.put("status", status);
         header.put("docid", documentId);
         return this.knobstickServiceImp.updateStatus(header.toString());
+    }
+
+    public void GetDocuments() {
+        // Get list documents sent from VPCP
+        LOGGER.info("Invoke GetDocuments from VPCP !!!!!");
+        JSONObject getDocumentsHeader = new JSONObject();
+        getDocumentsHeader.put("servicetype", "eDoc");
+        getDocumentsHeader.put("messagetype", MessageType.edoc);
+        GetReceivedEdocResult getReceivedEdocResult = this.knobstickServiceImp.getReceivedEdocList(getDocumentsHeader.toString());
+        if (getReceivedEdocResult != null) {
+            LOGGER.info(pStart + "status:" + getReceivedEdocResult.getStatus());
+            LOGGER.info(pStart + "Desc:" + getReceivedEdocResult.getErrorDesc());
+            LOGGER.info(pStart + "Size:" + getReceivedEdocResult.getKnobsticks().size());
+            if (getReceivedEdocResult.getKnobsticks().size() > 0) {
+                for (Knobstick item : getReceivedEdocResult.getKnobsticks()) {
+                    try {
+                        LOGGER.info("Prepare get document from VPCP with docId: " + item.getId() + " from " + item.getFrom() + " to " + item.getTo());
+                        JSONObject getDocumentHeader = new JSONObject();
+                        String attachmentDir = PropsUtil.get("VPCP.attachment.dir");
+                        getDocumentHeader.put("filePath", attachmentDir);
+                        getDocumentHeader.put("docId", item.getId());
+                        GetEdocResult getEdocResult = this.knobstickServiceImp.getEdoc(getDocumentHeader.toString());
+                        JSONObject headerChangeStatus = new JSONObject();
+                        headerChangeStatus.put("docid", item.getId());
+                        if (getEdocResult != null) {
+                            LOGGER.info(pStart + "status:" + getEdocResult.getStatus());
+                            LOGGER.info(pStart + "Desc:" + getEdocResult.getErrorDesc());
+                            LOGGER.info(pStart + "file:" + getEdocResult.getFilePath());
+                            EdocDocument document = null;
+                            if (getEdocResult.getStatus().equals("OK")) {
+                                // TODO insert to database
+                                //parse data from edxml
+                                File file = new File(getEdocResult.getFilePath());
+                                InputStream inputStream = new FileInputStream(file);
+                                Ed ed = EdXmlParser.getInstance().parse(inputStream);
+                                LOGGER.info("Parser success document from file " + getEdocResult.getFilePath());
+                                //Get message header
+                                MessageHeader messageHeader = (MessageHeader) ed.getHeader().getMessageHeader();
+                                // create trace header list
+                                //Get trace header list
+                                TraceHeaderList traceHeaderList = ed.getHeader().getTraceHeaderList();
+                                //Get attachment
+                                List<Attachment> attachments = ed.getAttachments();
+                                //filter list organ of current organ on esb
+                                List<Organization> thisOrganizations = filterOrgan(messageHeader.getToes());
+                                messageHeader.setToes(thisOrganizations);
+                                StringBuilder documentEsbId = new StringBuilder();
+                                List<Error> errors = new ArrayList<>();
+                                List<AttachmentCacheEntry> attachmentCacheEntries = new ArrayList<>();
+                                // only check exist with new document
+                                if (EdocDocumentServiceUtil.checkNewDocument(traceHeaderList)) {
+                                    // check exist document
+                                    if (EdocDocumentServiceUtil.checkExistDocument(messageHeader.getDocumentId())) {
+                                        LOGGER.info("Exist document with document id " + messageHeader.getDocumentId() + " on Esb !!!!!");
+                                    } else {
+                                        LOGGER.info("--------- Prepare to save the document to the database ------ " + messageHeader.getDocumentId());
+                                        document = EdocDocumentServiceUtil.addDocument(messageHeader,
+                                                traceHeaderList, attachments, documentEsbId, attachmentCacheEntries, errors);
+                                    }
+                                } else {
+                                    LOGGER.info("--------- Prepare to save the document to the database ------ " + messageHeader.getDocumentId());
+                                    document = EdocDocumentServiceUtil.addDocument(messageHeader,
+                                            traceHeaderList, attachments, documentEsbId, attachmentCacheEntries, errors);
+                                }
+                                if (document != null) {
+                                    LOGGER.info("Save document from vpcp successfully from file " + getEdocResult.getFilePath() + " to database !!!!!!!!");
+                                    document.setDocumentExtId(item.getId());
+                                    document.setReceivedExt(true);
+                                    EdocDocumentServiceUtil.updateDocument(document);
+                                    LOGGER.info("Update document from vpcp successfully for document " + documentEsbId.toString());
+                                    // TODO change status to vpcp
+                                    headerChangeStatus.put("status", "done");
+                                } else {
+                                    LOGGER.error("Error save document from vpcp  with document code " + messageHeader.getCode());
+                                    headerChangeStatus.put("status", "fail");
+                                }
+                            } else {
+                                headerChangeStatus.put("status", "fail");
+                            }
+                            GetChangeStatusResult getChangeStatusResult = this.knobstickServiceImp.updateStatus(headerChangeStatus.toString());
+                            LOGGER.info("Confirm receiver for document --------------> " + item.getId());
+                            if (getChangeStatusResult != null) {
+                                LOGGER.info("Confirm status ---------------> " + getChangeStatusResult.getStatus());
+                                LOGGER.info("Confirm error code  ---------------> " + getChangeStatusResult.getErrorCode());
+                                LOGGER.info("Confirm error code  ---------------> " + getChangeStatusResult.getErrorCode());
+                            } else {
+                                LOGGER.error("Error confirm receiver for document of vpcp with id " + item.getId());
+                            }
+                        }
+                    } catch (Exception e) {
+                        LOGGER.error("Error get list documents from VPCP " + e);
+                    }
+                }
+            }
+        } else {
+            LOGGER.error("Get list document from vpcp null !!!!");
+        }
+    }
+
+    private List<Organization> filterOrgan(List<Organization> organizations) {
+        List<String> organs = new ArrayList<>();
+        List<Organization> result = new ArrayList<>();
+        organizations.forEach(o -> organs.add(o.getOrganId()));
+        List<EdocDynamicContact> dynamicContacts = EdocDynamicContactServiceUtil.getContactsByMultipleDomains(organs);
+        if (dynamicContacts.size() > 0) {
+            List<EdocDynamicContact> agencies = dynamicContacts.stream().filter(EdocDynamicContact::getAgency).collect(Collectors.toList());
+            if (agencies.size() > 0) {
+                List<String> organDomains = agencies.stream().map(EdocDynamicContact::getDomain).collect(Collectors.toList());
+                for (Organization domain : organizations) {
+                    if (organDomains.contains(domain.getOrganId())) {
+                        result.add(domain);
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    public void GetDocumentsTest() throws IOException, ParserException {
+        // Get list documents sent from VPCP
+        String filePath = "/home/quangcv/backup-school/edoc-integrator/edoc-webapp/src/main/resources/e5f4e280-55b3-460f-be38-e57b80bc3b0d.edxml";
+        //parse data from edxml
+        File file = new File(filePath);
+        InputStream inputStream = new FileInputStream(file);
+        Ed ed = EdXmlParser.getInstance().parse(inputStream);
+        //Get message header
+        MessageHeader messageHeader = (MessageHeader) ed.getHeader().getMessageHeader();
+        //filter list organ of current organ on esb
+        List<Organization> thisOrganizations = filterOrgan(messageHeader.getToes());
+        messageHeader.setToes(thisOrganizations);
+        //Get attachment
+        List<Attachment> attachments = ed.getAttachments();
+        // create trace header list
+        //Get trace header list
+        TraceHeaderList traceHeaderList = ed.getHeader().getTraceHeaderList();
+        String businessInfo = CommonUtil.getBusinessInfo(traceHeaderList);
+        // TODO change status to vpcp
+    }
+
+    public static void main(String[] args) throws IOException, ParserException {
+        ServiceVPCP.getInstance().GetDocumentsTest();
     }
 
     static {
--- edoc-task/src/main/java/com/bkav/edoc/task/center/EdocGetDocumentTask.java
+++ edoc-task/src/main/java/com/bkav/edoc/task/center/EdocGetDocumentTask.java
@@ -1,23 +1,32 @@
 package com.bkav.edoc.task.center;
 
 import com.bkav.edoc.service.kernel.util.GetterUtil;
-import com.bkav.edoc.task.util.PropsUtil;
+import com.bkav.edoc.service.xml.base.util.DateUtils;
 import org.apache.log4j.Logger;
 import org.apache.synapse.ManagedLifecycle;
 import org.apache.synapse.core.SynapseEnvironment;
 import org.apache.synapse.task.Task;
 
+import java.util.Date;
+
 public class EdocGetDocumentTask implements Task, ManagedLifecycle {
     private SynapseEnvironment synapseEnvironment;
+    private String turnOn;
 
     /**
      * Execute method will be invoked by the QuartzJOb.
      */
     @Override
     public void execute() {
-        boolean isTurnOn = GetterUtil.getBoolean(PropsUtil.get("edoc.task.get.document.vpcp.turn.on"), false);
-
+        boolean isTurnOn = GetterUtil.getBoolean(this.getTurnOn(), false);
         LOGGER.info("------------------------ Edoc Get Documents Task Invoke -----------------------");
+        if (isTurnOn) {
+            LOGGER.info("Start get documents from vpcp at " + DateUtils.format(new Date(), DateUtils.DEFAULT_DATETIME_FORMAT));
+
+        } else {
+            LOGGER.info("------------------------ Get Documents from VPCP turn off -----------------------");
+        }
+
     }
 
     /**
@@ -38,6 +47,22 @@ public class EdocGetDocumentTask implements Task, ManagedLifecycle {
     @Override
     public void destroy() {
 
+    }
+
+    public SynapseEnvironment getSynapseEnvironment() {
+        return synapseEnvironment;
+    }
+
+    public void setSynapseEnvironment(SynapseEnvironment synapseEnvironment) {
+        this.synapseEnvironment = synapseEnvironment;
+    }
+
+    public String getTurnOn() {
+        return turnOn;
+    }
+
+    public void setTurnOn(String turnOn) {
+        this.turnOn = turnOn;
     }
 
     private final static Logger LOGGER = Logger.getLogger(EdocGetDocumentTask.class);
